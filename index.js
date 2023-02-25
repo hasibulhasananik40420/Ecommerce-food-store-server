4dfdd06f1516b3a8a0c8a1acc594c16eed1a5cdc
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-02-25 23:35:29 +0600

    jwt token done

--- index.js
+++ index.js
@@ -19,12 +19,39 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+//verifyJWT jwt token
+
+function verifyJWT(rrq, res, next) {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" })
+    }
+
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbiden access" })
+        }
+        res.decoded = decoded
+        next()
+    })
+}
+
+
+
+
+
+
+
+
 
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db("productCollection").collection("products");
         const cartCollection = client.db("cartCollection").collection("cart");
+        const userCollection = client.db("userCollection").collection("user");
 
 
         //  allproducts
@@ -46,11 +73,18 @@ async function run() {
 
         // add to cart
 
-        app.put('/addtocart/:id', async (req, res) => {
+        app.post('/addtocart/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
+            const email = req.query.email
             const Cart = req.body
+            const filter = {
+                $and: [
+                    { _id: new ObjectId(id) },
+                    { 'userEmail': email }
+                ]
+            };
 
+            console.log(filter)
             const options = { upsert: true };
             const updatedDoc = {
                 $set: Cart
@@ -86,6 +120,67 @@ async function run() {
             const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
+
+
+        // save all user in database
+        app.put('users/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            console.log(token)
+            res.send({ result, token })
+
+        })
+
+        // app.get('/jwt', async (req, res) => {
+        //     const email = req.query.email
+        //     const query = { email: email }
+        //     console.log(query)
+        //     const user = await userCollection.findOne(query)
+        //     if (user) {
+        //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+        //         return res.send({ accessToken: token })
+        //     }
+        //     console.log(user)
+        //     res.status(403).send({ accessToken: '' })
+        // })
+
+
+        // app.post('/users', async (req, res) => {
+        //     const user = req.body
+        //     const result = await userCollection.insertOne(user)
+        //     res.send(result)
+        // })
+
+
+        // male admin
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
+        // role set
+
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updatedDoc = {
+                $set: { role: 'admin' }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
+
 
     }
 
